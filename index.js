70deb0ee55527cf7bf34cd7b2db22dc15345b73c
billautomata <bill@evident.io>
2014-10-07 22:42:47 -0700

    added socket.io

--- index.js
+++ index.js
@@ -5,14 +5,26 @@ var port = process.env.PORT || 8000
 
 // initalize the server
 var app = express()
+
+var server = require('http').Server(app)
+
+// start the server
+var server = app.listen(port, function(err){
+  if(err){ console.log(err) }
+  console.log('Listening on port ' + port)
+})
+var io = require('socket.io')(server)
+
 app.engine('handlebars', exphbs( {defaultLayout: 'main'} ));
 app.set('view engine', 'handlebars');
 
 // set up all the routes
 require('./routes/route_index.js')(app)
 
-// start the server
-app.listen(port, function(err){
-  if(err){ console.log(err) }
-  console.log('Listening on port ' + port)
+// set up socket.io
+io.on('connection', function(socket){
+
+  console.log('connection')
+  socket.emit('intro', {yep: 1})
+
 })
